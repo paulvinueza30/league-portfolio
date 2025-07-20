013ee9601d21bf1bba02d09d94e9f6dad54ff78e
Paul Vinueza <paulvinueza30@gmail.com>
2025-07-19 22:38:26 -0400

    queue info component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import "./App.css";
 // import RuneSelection from "@/components/runes";
 // import SummonerSpells from "./components/summoner-spells";
-import BMSection from "./components/bm-section";
+// import BMSection from "./components/bm-section";
+import QueueInfo from "./components/queueInfo";
 
 function App() {
   return (
     <>
       <div className="flex justify-center items-center border-8 h-screen">
-        <BMSection />
+        <QueueInfo />
       </div>
     </>
   );
---
+++ src/components/queueInfo/index.tsx
@@ -0,0 +1,8 @@
+export default function QueueInfo() {
+  return (
+    <div className="flex flex-col items-end font-bold text-xl text-[#63686C]">
+      <p>1 v 1</p>
+      <p>Portfolio Review</p>
+    </div>
+  );
+}
