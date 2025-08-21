341567291a79249260338456c085d2e0c0bbe574
Paul Martin Vinueza <paulvinueza30@gmail.com>
2025-08-20 23:52:08 -0400

    added a banner for lock in intro

--- src/components/match-intro/index.tsx
+++ src/components/match-intro/index.tsx
@@ -2,19 +2,20 @@ import { lockInAtom } from "@/atoms/champAtom";
 import CountdownFrame from "./CountdownFrame";
 import { useAtom } from "jotai";
 
-function IntroBanner() {
+function Banner({isLockedIn}: {isLockedIn:boolean}){
   return (
     <h1 className="uppercase font-bold text-4xl text-center absolute top-5 left-1/2 -translate-x-1/2 z-10 match-intro-item">
-      Ready to code and carry!
+      {isLockedIn ? "Check out my art/pictures!" : "Ready to code and carry!"}
     </h1>
   );
 }
+
 
 export default function MatchIntro() {
   const [isLockedIn] = useAtom(lockInAtom);
   return (
     <div className="flex flex-col mi justify-center text-[#ECE4D0] w-full">
-      {!isLockedIn && <IntroBanner />}
+      <Banner isLockedIn={isLockedIn}></Banner>
 
       <CountdownFrame key={isLockedIn ? "locked" : "unlocked"} />
     </div>
