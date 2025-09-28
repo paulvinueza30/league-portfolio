279dd529481dd0fb905337995897205e7e80bbb6
Paul Martin Vinueza <paulvinueza30@gmail.com>
2025-09-28 17:33:09 -0400

    edge runtime is dumb

--- api/progress/index.ts
+++ api/progress/index.ts
@@ -1,5 +1,5 @@
 // api/progress/index.ts
-import getProgress, { apiKeys } from "./registry";
+import getProgress, { apiKeys } from "./registry.js";
 
 export async function GET(request: Request) {
   try {
