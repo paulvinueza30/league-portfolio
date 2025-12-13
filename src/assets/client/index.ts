735b21aac70fc3c92c7c47d74fc9e69efb4eb037
Paul Martin Vinueza <paulvinueza30@gmail.com>
2025-12-13 09:30:43 -0500

    changed picture of starting screen

--- src/assets/client/index.ts
+++ src/assets/client/index.ts
@@ -1,2 +1,2 @@
 export { default as queueBackground } from "./bgqueue.jpg";
-export { default as inQueueuBackground } from "./in-queue.jpeg";
+export { default as inQueueuBackground } from "./in-queue.jpg";
