078db97c3be6a5b45a2f04d58a1fe206a16016f2
Paul Martin Vinueza <paulvinueza30@gmail.com>
2026-01-06 03:31:04 -0500

    footer fix

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <footer className="w-full border-t border-[#463714] bg-[#0A1428] py-8 mt-12">
       <div className="mx-auto max-w-4xl px-6 flex flex-col items-center gap-4">
         <p className="text-sm text-[#CDBE91]">
-          On the journey to getting cracked, {currentYear} 
+          On the journey to getting cracked, {currentYear}.
         </p>
       </div>
     </footer>
