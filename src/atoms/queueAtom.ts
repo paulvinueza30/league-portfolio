39d25a0e03d76ba54023cfcc4897ded82418811c
paul vinueza <paulvinueza30@gmail.com>
2026-01-22 21:29:18 -0500

    fix(animations): create and export disableAnimationsAtom
    - Create and export the 'disableAnimationsAtom' in 'queueAtom.ts'.
    - This resolves the import error in 'AudioSetupModal.tsx' and 'QueuePop.tsx'.

--- src/atoms/queueAtom.ts
+++ src/atoms/queueAtom.ts
@@ -2,3 +2,4 @@ import { atom } from "jotai";
 
 export const audioModalAtom = atom<boolean>(false);
 export const acceptedAtom = atom<boolean>(false);
+export const disableAnimationsAtom = atom<boolean>(false);
