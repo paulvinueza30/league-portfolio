df3dea4cf75958b5c17d370a7bc0f173cc155a3f
paul vinueza <paulvinueza30@gmail.com>
2026-01-24 18:49:45 -0500

    select none for blog cards

--- frontend/src/components/blog/BlogCard.tsx
+++ frontend/src/components/blog/BlogCard.tsx
@@ -7,7 +7,7 @@ interface BlogCardProps {
 
 export default function BlogCard({ post }: BlogCardProps) {
   return (
-    <Card className="w-full bg-[#010A13] border-2 border-[#463714] transition-all hover:border-[#C89B3C] hover:bg-[#1E272C]">
+    <Card className="w-full bg-[#010A13] border-2 border-[#463714] transition-all hover:border-[#C89B3C] hover:bg-[#1E272C] select-none">
       <CardHeader>
         <div className="mb-2 text-sm text-[#BCAC88]">
           {new Date(post.date).toLocaleDateString("en-US", {
