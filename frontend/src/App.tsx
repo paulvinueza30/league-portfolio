ca4d49e32f7f3a3a8d9f7950ffbc2cd1da3a3bc5
paul vinueza <paulvinueza30@gmail.com>
2026-01-25 01:10:16 -0500

    add app page tracking with ackiee

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,9 +8,24 @@ import ChampSelectScreen from "./components/champ-select-screen";
 import { AudioProvider } from "./context/AudioContext";
 import DeclinePage from "./pages/DeclinePage";
 
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+const RouteTracker = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if ((window as any).ackeeTracker) {
+      (window as any).ackeeTracker.record("4a74bed1-45d8-4bf6-b1f9-4aa42581dad7");
+    }
+  }, [location.pathname]);
+
+  return null;
+};
 export default function App() {
   return (
     <AudioProvider>
+      <RouteTracker />
       <Routes>
         <Route path="/" element={<InQueue />} />
         <Route path="/champ-select" element={<ChampSelectScreen />} />
